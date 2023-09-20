d6a51a542a6d925b33c2228df5c31814ed777965
Adrian Ramirez <adrian.ramirez45679@gmail.com>
2023-09-20 11:08:57 -0700

    intro challenges

---
+++ intro-adrian-shaun.rb
@@ -0,0 +1,40 @@
+=begin
+Terminal:
+Open the IRB terminal. Exit the terminal and reopen the terminal.
+Add, subtract, multiply, and divide integers.
+Add, subtract, multiply, and divide floats.
+Find the remainder of dividing two numbers using the modulo operator (%).
+Divide an integer by 0.
+Divide a float by 0.
+Divide 0 by 0.
+Create a variable and assign an integer.
+Calculate the variable divided by 2.
+Find the remainder of the variable when divided by 3.
+Create another variable and assign it the integer 13.
+Use the relational operators on the two variables.
+Reassign the value of one variable to be 7.
+Reassign the value of one variable to be 26 times its current value.
+=end
+
+my_num = 12
+puts "There are #{my_num} years old."
+
+my_name = "Shaun"
+puts my_name.upcase
+puts my_name.reverse
+
+
+
+
+tv_shows = ["CNN", "Family Guy", "Netlix", "Fox", "One Piece"];
+
+puts tv_shows.length
+puts tv_shows.first
+puts tv_shows[3]
+
+puts tv_shows.reverse!
+
+top_tv_shows = []
+top_tv_shows[0] = tv_shows[4]
+top_tv_shows[1] = tv_shows[1]
+puts top_tv_shows
