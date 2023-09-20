8f17ed414a705552809515ccf4825974cfedcf46
XenaSit <sitnikovaxena@gmail.com>
2023-09-20 14:43:02 -0700

    finnished 1st 4 challenges

--- ruby-intro-xena-mal.rb
+++ ruby-intro-xena-mal.rb
@@ -1,7 +1,7 @@
 # name = 'Billy'
 # puts "My name is #{name}"
 
-var = 'we are awesome'
+# var = 'we are awesome'
 
 # p var.upcase
 # p var.reverse
@@ -9,4 +9,34 @@ var = 'we are awesome'
 # p var.include?('h')
 # p var.capitalize
 # p var.delete("are")
-p var.index[]
+# p var.index[]
+
+# def sum_these_numbers num1, num2
+#     num1 + num2
+# end
+# p sum_these_numbers(1, 4)
+
+# def is_even num
+#     num % 2 == 0
+# end
+# p is_even(7)
+
+# def one_and_ten num
+#     if num >= 1 && num <= 10
+#         "Valid"
+#     else
+#         "Invalid"
+#     end
+# end
+# p one_and_ten(15)
+
+
+
+def palindrome string
+    if string == string.reverse
+        "it is a palidrome"
+    else 
+       " it is not"
+    end
+end
+p palindrome ("racecar")
