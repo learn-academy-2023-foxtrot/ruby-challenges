1b2300d01716a64276ec57078030d25a8b6d26af
malsherer <mgracesherer@gmail.com>
2023-09-20 11:06:47 -0700

    started challenges - we love ruby :)

---
+++ ruby-intro-xena-mal.rb
@@ -0,0 +1,12 @@
+# name = 'Billy'
+# puts "My name is #{name}"
+
+var = 'we are awesome'
+
+# p var.upcase
+# p var.reverse
+# p var.include?('we')
+# p var.include?('h')
+# p var.capitalize
+# p var.delete("are")
+p var.index[]
