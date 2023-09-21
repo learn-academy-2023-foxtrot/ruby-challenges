a7021391c34d84b2a192b5db91214b4682859e74
Varelazi <ilenevarela03@gmail.com>
2023-09-21 16:49:01 -0700

    almost finished working on last two

--- classes-ilene-xena.rb
+++ classes-ilene-xena.rb
@@ -38,19 +38,42 @@ laundry.task_completion = 'done'
 make_bed.task_completion = 'done'
 make_coffe.task_completion = 'done'
 
-p laundry
-p make_bed
-p make_coffe
+# p laundry
+# p make_bed
+# p make_coffe
 
 # For the following ColorPalette challenge use initialize and attr_accessor methods in your class.
 
-
-
 # As a developer, I can create a class called ColorPalette.
+class ColorPalette
+  attr_accessor(:color)
+  def initialize (color1, color2, color3)
+    @color = color1, color2, color3
+  end
+
+  def all_colors
+    "My favorite shades are #{color}"
+  end
+end
+
 # As a developer, I can create three instances (objects) of class ColorPalette.
 # e.g green = ColorPalette.new
 # As a developer, I can initialize each instance of the class ColorPalette with three colors.
-# e.g. green = ColorPalette.new('Chartreuse', 'Kelly', 'Seafoam')
 # As a developer, I can print the value of each individual color.
+# e.g. green = ColorPalette.new('Chartreuse', 'Kelly', 'Seafoam')
+green = ColorPalette.new('Chartreuse', 'Kelly', 'Seafoam')
+blue = ColorPalette.new('Light Blue','Navy Blue','Turquoise')
+pink = ColorPalette.new('Orchid','Rose','Peach')
+
+p green.color
+p blue.color
+p pink.color
+p green.all_colors
+p blue.all_colors
+p pink.all_colors
+
+
 # As a developer, I can create a method called all_colors that when called will print a sentence telling me the three colors of a given palette.
+
+
 # As a developer, I can change one or more colors of a given palette.
