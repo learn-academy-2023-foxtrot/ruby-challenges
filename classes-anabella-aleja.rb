0d5b1dc114dfc9ca83a08cdc01284106183163d6
aesandino <anabellasandino@gmail.com>
2023-09-21 16:48:18 -0700

    completed both challenges

--- classes-anabella-aleja.rb
+++ classes-anabella-aleja.rb
@@ -29,10 +29,56 @@ p dishes
 # For the following ColorPalette challenge use initialize and attr_accessor methods in your class.
 
 # As a developer, I can create a class called ColorPalette.
+class ColorPalette
+end
+
 # As a developer, I can create three instances (objects) of class ColorPalette.
+pink = ColorPalette.new
+shade_1 = ColorPalette.new
+shade_2 = ColorPalette.new
+
 # e.g green = ColorPalette.new
 # As a developer, I can initialize each instance of the class ColorPalette with three colors.
+# class ColorPalette 
+#     def initialize (pink, shade_1, shade_2)
+#         @pink = pink
+#         @shade_1 = shade_1
+#         @shade_2= shade_2
+#     end
+# end
+# pink = ColorPalette.new('Fucshia', 'Hot Pink', 'Pale Pink')
+# p pink
 # e.g. green = ColorPalette.new('Chartreuse', 'Kelly', 'Seafoam')
 # As a developer, I can print the value of each individual color.
+# class ColorPalette 
+#     attr_accessor(:pink, :shade_1, :shade_2)
+#     def initialize (pink, shade_1, shade_2)
+#         @pink = pink
+#         @shade_1 = shade_1
+#         @shade_2= shade_2
+#     end
+# end
+# pink = ColorPalette.new('Fucshia', 'Hot Pink', 'Pale Pink')
+# p pink.shade_1
+
+
 # As a developer, I can create a method called all_colors that when called will print a sentence telling me the three colors of a given palette.
+class ColorPalette 
+    attr_accessor(:pink, :shade_1, :shade_2)
+    def initialize (pink, shade_1, shade_2)
+        @pink = pink
+        @shade_1 = shade_1
+        @shade_2= shade_2
+    end
+    def all_colors 
+        "This palette includes the colors #{pink} #{shade_1} #{shade_2}"
+    end
+end
+
+pink = ColorPalette.new('Fucshia', 'Hot Pink', 'Pale Pink')
+p pink.all_colors
+
 # As a developer, I can change one or more colors of a given palette.
+
+pink.shade_1 = 'rose'
+p pink
