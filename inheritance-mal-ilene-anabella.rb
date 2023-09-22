84a68491d0a3a42cb05cbddb11419f47910e344d
Varelazi <ilenevarela03@gmail.com>
2023-09-22 13:07:31 -0700

    got through most of the challenge

--- inheritance-mal-ilene-anabella.rb
+++ inheritance-mal-ilene-anabella.rb
@@ -19,14 +19,25 @@ class Fish < Animal
         @species = species
     end
 end
+# salmon = Fish.new('salmon', 'atlantic')
+#p salmon
+# salmon.aging
+# p salmon.getinfo
 
-salmon = Fish.new('salmon', 'atlantic')
-salmon.aging
-p salmon.getinfo
+class Mammal < Animal
+    def initialize(name, species)
+        super(name)
+        @body_tempurature = 'warm blooded'
+        @species = species
+    end
+end
+bear = Mammal.new('bear','Black Bear')
+p bear
+bear.aging
+p bear.getinfo
 
+cat = Mammal.new('cat','Cheetah')
+p cat
+cat.aging
+p cat.getinfo
 
-        
-
-# cat = Animal.new('cat')
-# cat.aging
-# p cat.getinfo
