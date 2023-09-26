c1efe2b6f79941e344971443f08ed0c0f706756a
malsherer <mgracesherer@gmail.com>
2023-09-26 12:00:55 -0700

    worked on stretch goals

--- rspec-ilene-mal/rspec.rb
+++ rspec-ilene-mal/rspec.rb
@@ -1,6 +1,8 @@
+require 'date'
+
 class Task 
     attr_accessor :title, :description, :status, :due_date
-    def initialize status = 'in progress'
+    def initialize title, description, status = 'in progress', due_date
         @title = title 
         @description = description
         @status = status
@@ -9,4 +11,14 @@ class Task
     def update 
         @status = 'complete'
     end
+    def task_list
+        p "Task List: #{title}"
+    end
 end
+# list = Task.new
+# p list
+
+vacuum = Task.new('Vacuum', 'vacuuming', Date.new(2023,9,26))
+p vacuum
+vacuum.task_list
+
