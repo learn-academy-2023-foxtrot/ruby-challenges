9f119f98e4d690ac80b21be07a3f7ea50c9b47c0
Adrian Ramirez <adrian.ramirez45679@gmail.com>
2023-09-26 12:00:32 -0700

    rspec challenges

---
+++ rspec-adrian-xe/rspec.rb
@@ -0,0 +1,20 @@
+class Task
+    attr_accessor :title, :description, :status, :due_date
+
+    def initialize
+        @title = ''
+        @description = ''
+        @status = 'in progress'
+        @due_date = nil
+    end
+
+    def mark_done
+        @status = done
+    end
+
+    def to_s
+        result = "Title: #{@title} Description: #{@description} Status: {@status}"
+        result += "Due Date: #{due_date}" if @due_date
+        result
+    end
+end
